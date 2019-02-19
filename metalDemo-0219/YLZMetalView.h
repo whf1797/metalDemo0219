023a2e780b4f2d52398946fbe158b451f9909a69
王洪飞 <whf@wanghongfeideMacBook-Pro.local>
2019-02-20 00:16:41 +0800

    first commit

---
+++ metalDemo-0219/YLZMetalView.h
@@ -0,0 +1,17 @@
+//
+//  YLZMetalView.h
+//  metalDemo-0219
+//
+//  Created by 王洪飞 on 2019/2/19.
+//  Copyright © 2019 王洪飞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface YLZMetalView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ metalDemo-0219/YLZMetalView.m
@@ -0,0 +1,21 @@
+//
+//  YLZMetalView.m
+//  metalDemo-0219
+//
+//  Created by 王洪飞 on 2019/2/19.
+//  Copyright © 2019 王洪飞. All rights reserved.
+//
+
+#import "YLZMetalView.h"
+
+@implementation YLZMetalView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
